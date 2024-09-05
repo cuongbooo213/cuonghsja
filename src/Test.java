979aa21b3b76c0e867711583d427aa950ff16492
Your Name <cuongbooo213>
2024-09-05 09:26:21 +0700

    new

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,6 @@
-public class App {
+public class Test {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("cuong dai ca");  
     }
 }
